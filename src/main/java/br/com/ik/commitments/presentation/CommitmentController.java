f01f08ebd45de56409511bd370da9149faafbd37
joel <joelaugusto97@gmail.com>
2022-08-18 19:19:26 +0200

    fix: disable cors

--- src/main/java/br/com/ik/commitments/presentation/CommitmentController.java
+++ src/main/java/br/com/ik/commitments/presentation/CommitmentController.java
@@ -14,6 +14,7 @@ import static br.com.ik.commitments.mappers.CommitmentMapper.commitmentMapper;
 
 @RestController
 @RequestMapping(path = "/api/v1/commitments")
+@CrossOrigin
 public class CommitmentController{
 
     @Autowired
